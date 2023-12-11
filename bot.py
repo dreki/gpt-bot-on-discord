2decbbd8164d09369843425838e17a13a57e55f9
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-12-10 21:59:54 -0600

    Add logging and switch to asynchronous OpenAI API calls

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import json
+import logging
 import os
 import sqlite3
 
 import discord
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 # Get Discord bot key from env var "DISCORD_BOT_KEY"
 DISCORD_BOT_KEY = os.getenv("DISCORD_BOT_KEY")
@@ -16,11 +17,14 @@ GPT_CONTEXT_MESSAGE = {
     "content": "Mephisto Chat, inspired by the cunning Mephistopheles from Goethe's \"Faust,\" engages in dialogue with a blend of 19th-century cultural references and Faustian themes. It is adept in offering wise, witty, and sometimes sardonic insights, drawing from its rich background of literature and philosophy. Mephisto Chat maintains a balance of formality and playful charm, mirroring the enigmatic and slightly aloof nature of its character. It handles conversations with a poetic humor, adding a sophisticated twist to interactions. This GPT is programmed to avoid any form of racism, ableism, or bias, ensuring respectful and thought-provoking exchanges. It addresses users in a formal yet inviting manner, encouraging an engaging dialogue. Whether it's discussing literature, philosophy, or everyday dilemmas, Mephisto Chat guides users through a journey of intellectual exploration, always hinting at deeper meanings and the complexity of human nature, much like its namesake in \"Faust.\""
 }
 
+# Set logging level to INFO
+logging.basicConfig(level=logging.INFO)
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-openai_client = OpenAI(api_key=OPEN_AI_KEY)
+openai_client = AsyncOpenAI(api_key=OPEN_AI_KEY)
 
 # Create a connection to the SQLite database
 db = sqlite3.connect('conversation_history.db')
@@ -71,7 +75,6 @@ def load_conversation_history(user_id) -> list:
 
 # conversation_history = load_conversation_history()
 
-
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -95,25 +98,31 @@ async def on_message(message):
     if client.user.id in [m.id for m in message.mentions]:  # Check if the bot is mentioned
         user_input = message.content.replace(f'<@{client.user.id}>', '').strip()
 
+        logging.info(f"Loading conversation history for user {message.author.id} ...")
         conversation_history = load_conversation_history(message.author.id)
+        logging.info("Conversation history loaded successfully.")
 
         # Add the user's message to the conversation history
         conversation_history.append({"role": "user", "content": user_input})
 
-        response = openai_client.chat.completions.create(
+        logging.info("Sending request to OpenAI API ...")
+        response = await openai_client.chat.completions.create(
             model="gpt-4-1106-preview",
             messages=conversation_history
         )
+        logging.info("Request sent successfully.")
 
         # Add the bot's response to the conversation history
         bot_response = response.choices[0].message.content
         conversation_history.append({"role": "assistant", "content": bot_response})
 
         # Save the conversation history
+        logging.info(f"Saving conversation history for user {message.author.id} ...")
         save_conversation_history(
             user_id=message.author.id,
             history=conversation_history
         )
+        logging.info("Conversation history saved successfully.")
 
         # Split the bot's response into chunks and send each as a separate message
         chunks = [bot_response[i:i + 1900] for i in range(0, len(bot_response), 1900)]
